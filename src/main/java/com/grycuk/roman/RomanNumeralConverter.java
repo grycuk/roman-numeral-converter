5fa744061de1273d881717bd5078cc515abcbc8a
Paul Grycuk <paul.grycuk@crowdcube.com>
2022-03-02 22:02:16 +0000

    Test for converting 1 to I

---
+++ src/main/java/com/grycuk/roman/RomanNumeralConverter.java
@@ -0,0 +1,8 @@
+package com.grycuk.roman;
+
+public class RomanNumeralConverter {
+
+    public String convert(int i) {
+        return null;
+    }
+}
---
+++ src/test/java/com/grycuk/roman/RomanNumeralConverterTest.java
@@ -0,0 +1,21 @@
+package com.grycuk.roman;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RomanNumeralConverterTest {
+
+    private RomanNumeralConverter romanNumeralConverter;
+
+    @BeforeEach
+    void setUp() {
+        romanNumeralConverter = new RomanNumeralConverter();
+    }
+
+    @Test
+    void shouldConvert1ToI() {
+        assertThat(romanNumeralConverter.convert(1)).isEqualTo("I");
+    }
+}
