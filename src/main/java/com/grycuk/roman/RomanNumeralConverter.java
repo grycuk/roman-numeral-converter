a70c101f0c4ca784dbb3483812af964c7f0303b5
Paul Grycuk <paul.grycuk@crowdcube.com>
2022-03-02 22:15:21 +0000

    Code for converting 5 to V

--- src/main/java/com/grycuk/roman/RomanNumeralConverter.java
+++ src/main/java/com/grycuk/roman/RomanNumeralConverter.java
@@ -6,6 +6,11 @@ public class RomanNumeralConverter {
 
     public String convert(int i) {
         StringBuilder sb = new StringBuilder();
+
+        if (i == 5) {
+            sb.append("V");
+            i = -5;
+        }
         if (i == 4) {
             sb.append("IV");
             i = -4;
