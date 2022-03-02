97b690c90aa75af80eb92e7477f33a22fdf1f403
Paul Grycuk <paul.grycuk@crowdcube.com>
2022-03-02 22:10:27 +0000

    Code for converting 4 to IV

--- src/main/java/com/grycuk/roman/RomanNumeralConverter.java
+++ src/main/java/com/grycuk/roman/RomanNumeralConverter.java
@@ -6,6 +6,11 @@ public class RomanNumeralConverter {
 
     public String convert(int i) {
         StringBuilder sb = new StringBuilder();
+        if (i == 4) {
+            sb.append("IV");
+            i = -4;
+        }
+
         IntStream.range(0, i).forEach(num -> sb.append("I"));
         return sb.toString();
     }
