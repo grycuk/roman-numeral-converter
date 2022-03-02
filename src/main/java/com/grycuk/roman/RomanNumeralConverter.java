7119417364a73a6aca96ed46003eb718fb4a1251
Paul Grycuk <paul.grycuk@crowdcube.com>
2022-03-02 22:19:43 +0000

    Code for converting 6 to VI, fix bug with the decrement

--- src/main/java/com/grycuk/roman/RomanNumeralConverter.java
+++ src/main/java/com/grycuk/roman/RomanNumeralConverter.java
@@ -7,13 +7,13 @@ public class RomanNumeralConverter {
     public String convert(int i) {
         StringBuilder sb = new StringBuilder();
 
-        if (i == 5) {
+        if (i >= 5) {
             sb.append("V");
-            i = -5;
+            i -= 5;
         }
-        if (i == 4) {
+        if (i >= 4) {
             sb.append("IV");
-            i = -4;
+            i -= 4;
         }
 
         IntStream.range(0, i).forEach(num -> sb.append("I"));
